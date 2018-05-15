13d51b7b7823c0c240b9fb28fe5a7c5df94b9f4e
Tim McIver <tim@timmciver.com>
2018-05-14 20:55:09 -0400

    Got Applicative composition law test to pass.

--- src/test/scala/ApplicativeTest.scala
+++ src/test/scala/ApplicativeTest.scala
@@ -31,17 +31,22 @@ class ApplicativeTest extends FlatSpec with Matchers {
   }
 
   "An Applicative" should "satisfy the composition law." in {
-    val addOneA = maybeApplicative.pure((_: Int) + 1)
-    val doubleA = maybeApplicative.pure((i: Int) => i * 2)
+    val addOneA: Maybe[Int => Int] = maybeApplicative.pure((_: Int) + 1)
+    val doubleA: Maybe[Int => Int] = maybeApplicative.pure((i: Int) => i * 2)
     val xA = maybeApplicative.pure(1)
     def compose[A, B, C](f: A => B)(g: B => C): A => C = g compose f
-    def composeA[A, B, C] = maybeApplicative.pure(compose[A, B, C] _)
+    def composeA[A, B, C]: Maybe[(A => B) => (B => C) => (A => C)] = maybeApplicative.pure(compose[A, B, C] _)
 
-    maybeApplicative.apply[Int, Int](composeA[Int, Int, Int])(addOneA) should be (maybeApplicative.pure(addOne(x)))
-    // The above gives error:
-    // [error]  found   : dogs.Maybe.Maybe[(Int => Int) => ((Int => Int) => (Int => Int))]
-    // [error]  required: dogs.Maybe.Maybe[Int => Int]
-    // [error]     maybeApplicative.apply[Int, Int](composeA[Int, Int, Int])(addOneA) should be (maybeApplicative.pure(addOne(x)))
-    // [error]                                              ^
+    val left: Maybe[Int] =
+      maybeApplicative.apply[Int, Int](
+        //maybeApplicative.apply[(B => C, A => C](ff: F[(B => C) => (A => C)])(fa: F[B => C]): F[A => C]
+        maybeApplicative.apply[Int => Int, Int => Int](
+          // maybeApplicative.apply[A => B, (B => C) => (A => C)](ff: F[(A => B) => (B => C) => (A => C)])(fa: F[A => B]): F[(B => C) => (A => C)]
+          maybeApplicative.apply[Int => Int, (Int => Int) => (Int => Int)](composeA[Int, Int, Int])(addOneA))(doubleA))(xA)
+
+    val right: Maybe[Int] = maybeApplicative.apply(doubleA)(maybeApplicative.apply(addOneA)(xA))
+
+    left should be (right)
+
   }
 }
